63e01ed01c9ea0f07a19e8b227f10448df8577fa
Fathi <babayejuadesina@gmail.com>
2023-11-27 11:58:21 +0100

    chore: import assets

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  primary: {
+    default: "#001233",
+  },
+  secondary: {
+    100: "#C1C4CD",
+    200: "#707480",
+  },
+  brand: {
+    default: "#0466C8",
+    light: "#67A2DC",
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
